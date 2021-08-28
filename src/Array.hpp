76c5ca62193392511379f123e31dd5f6a1cbb8b2
Chris Petrovitch <cpetrovitch@gmail.com>
2021-08-28 16:40:40 -0400

    minor docs

--- src/Array.hpp
+++ src/Array.hpp
@@ -6,6 +6,15 @@
 #include <cuda_runtime.h>
 
 namespace kernels {
+
+/**
+ * @brief CUDA kernel that performs an inplace add
+ * 
+ * @tparam T data type (must support +)
+ * @param a array to add
+ * @param out array the inplace addition is performed on
+ * @param N size of a and out
+ */
 template <typename T> __global__ void addArray(T const *a, T *out, size_t N) {
     int tid = threadIdx.x + blockIdx.x * blockDim.x;
     while (tid < N) {
@@ -14,6 +23,14 @@ template <typename T> __global__ void addArray(T const *a, T *out, size_t N) {
     }
 }
 
+/**
+ * @brief CUDA kernel that sets an entire array to a single value.
+ * 
+ * @tparam T data type
+ * @param a array to be set
+ * @param value the value to set the array too
+ * @param N size of the array
+ */
 template <typename T> __global__ void setArray(T *a, T value, size_t N) {
     int tid = threadIdx.x + blockIdx.x * blockDim.x;
     while (tid < N) {
